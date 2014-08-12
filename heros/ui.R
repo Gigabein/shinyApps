fe187627ced3562aed91efbb1938419bc700c726
Gigabein <gigabein@gmail.com>
2014-08-11 22:04:14 -0600

    added text boxes
    for max # of heroes in each category. Need to take those textInput values and use them to initialize the max value of the sliders. Time for bed!

--- heros/ui.R
+++ heros/ui.R
@@ -1,13 +1,24 @@
 shinyUI(pageWithSidebar(
     headerPanel(""),
     sidebarPanel(
+        textInput(inputID = "vLow",
+                  label = "Max # low heroes:",
+                  value = 7
+                  ),
+        textInput(inputID = "vMed",
+                  label = "Max # med heroes:",
+                  value = 25
+                  ),
+        textInput(inputID = "vHigh",
+                  label = "Max # high heroes:",
+                  value = 5
+                  ),
         sliderInput(inputId = "nLow",
                     label = "Number of low heroes:",
                     min = 0,
-                   max = 7,
-                   step = 1,
+                    max = 7,
+                    step = 1,
                     value = 0),
- 
         sliderInput(inputId = "nMed",
                     label = "Number of medium heroes:",
                     min = 0,
